efcb6574914ae43df1b628cb19bbcde887af65e9
wei <wendyhsu09@gmail.com>
2024-06-01 22:32:36 +0800

    add: openai reply message

--- app/Http/Controllers/Api/LineBotController.php
+++ app/Http/Controllers/Api/LineBotController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
@@ -10,20 +11,21 @@ use GuzzleHttp\Client;
 class LineBotController extends Controller
 {
 
-   
-    // 回應訊息
+
+
     public function handleWebhook(Request $request)
     {
 
-        // 解析 LINE Webhook 事件
+       
         $events = json_decode($request->getContent(), true);
-        // echo $events;
         // dd($events);
-        // exit();
         foreach ($events['events'] as $event) {
             if ($event['type'] === 'message' && $event['message']['type'] === 'text') {
                 // 取得用戶發送的訊息
-                $userMessage = $event['message']['text'];
+                // $userMessage = $event['message']['text'];
+                // 取得用戶發送的訊息送到open ai
+                $userMessage = $this->getMessage($event['message']['text']);
+
 
                 // 回應用戶發送的訊息
                 $this->sendReplyMessage($event['replyToken'], $userMessage);
@@ -61,6 +63,44 @@ class LineBotController extends Controller
         return $response->getStatusCode();
     }
 
+    private function getMessage($message)
+    {
+        try {
+            $url = 'https://api.openai.com/v1/chat/completions';
+
+            $data = [
+                "messages" => [
+                    [
+                        "role" => "system",
+                        "content" => "你現在是一個貓的訂房網並且提供購物商品的功能，商品有關貓的物品，請到平台訂購"
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => $message
+                    ]
+                ],
+                "model" => "gpt-3.5-turbo-0613",
+                "temperature" => 0.7,
+                "max_tokens" => 150,
+                "top_p" => 1,
+                "stream" => false
+            ];
+
+            $httpClient = new Client();
+            $responseAi = $httpClient->post($url, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('OPEN_AI_KEY')
+                ],
+                'json' => $data
+            ]);
+
+            $responseBody = json_decode($responseAi->getBody(), true);
+            return $responseBody['choices'][0]['message']['content'] ?? '對不起，我無法理解你的意思';
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
     // --------------------------------------------------------------------------------------------------
 
 
